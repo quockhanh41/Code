5d1dcd61023604d47ca10683f0339ea70645bcd7
quockhanh41 <115342969+quockhanh41@users.noreply.github.com>
2024-10-08 23:42:04 +0700

    test

--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,9 +1,9 @@
 let display = (a, b) => {
-    console.log(a+b)
+  console.log(a + b)
 }
 // setTimeout(display(1, 4), 3000);
 
-setTimeout(() => "ilu", 3000);
+setTimeout(() => console.log("ilu"), 3000);
 
 function myFunction(value) {
   console.log(value);
